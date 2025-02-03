75e0b12651144ffdd8b62054458396b14e89cb58
Laaveniya <lavaniya.lala@gmail.com>
2025-02-03 17:33:59 +0530

    Refactor string sanitization into a method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,11 +2,16 @@
 
 class Calculator
   def add(string)
+    sum = 0
+    sanitize_string(string).split(',').each{ |number| sum += number.to_i }
+    sum
+  end
+
+  private
+
+  def sanitize_string(string)
     delimiter = "\n"
     delimiter = string.lines.first[2] if string.start_with?('//')
-    santized_string = string.gsub(delimiter, ',')
-    sum = 0
-    santized_string.split(',').each{ |number| sum += number.to_i }
-    sum
+    string.gsub(delimiter, ',')
   end
 end
